b43362d09b984594123402544bd514e9bf7722e0
Stuart Lambon <stuart@lambon.co.uk>
2018-05-26 13:46:39 +0100

    Add markup and stying for home/subscribe page

--- src/HomeHeader.js
+++ src/HomeHeader.js
@@ -4,7 +4,13 @@ import illustration from './assets/img/illustration.svg';
 class HomeHeader extends Component {
   render() {
     return (
-		<img src={illustration} alt="Dead Drop illustration showing an briefcase on a roadside" />
+		<div className="intro">
+			<h2 className="intro-header">
+				<span className="intro-header__subtitle">Get ready for the</span><br />
+				<span className="intro-header__title">Dead<br />Drop</span>
+			</h2>
+			<img className="intro__illustration" src={illustration} alt="Dead Drop illustration showing an briefcase on a roadside" />
+		</div>
     );
   }
 }
