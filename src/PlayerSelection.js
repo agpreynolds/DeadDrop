4a3ca0fb4f77354a696e2201d33cf005ac8c42de
Alex Reynolds <alexander.reynolds@myport.ac.uk>
2018-05-29 19:03:40 +0100

    Add more/less buttons for player selection

--- src/PlayerSelection.js
+++ src/PlayerSelection.js
@@ -6,11 +6,25 @@ class PlayerSelection extends Component {
         this.props.onSelectionChanged(event.target.value);        
     }
 
+    handleLessClicked = () => {
+        this.props.onSelectionChanged(this.props.noOfPlayers - 1);
+    }
+
+    handleMoreClicked = () => {
+        this.props.onSelectionChanged(this.props.noOfPlayers + 1);
+    }
+
     render() {
         return (
             <div>
                 <h2 className="page-title">Number of Players</h2>
-                <input type="number" name="players" value={this.props.noOfPlayers} onChange={this.handleChange}/>
+
+                <div>
+                    <button onClick={this.handleLessClicked} disabled={this.props.noOfPlayers < 3}>Less</button>
+                    <span>{this.props.noOfPlayers}</span>
+                    <button onClick={this.handleMoreClicked} disabled={this.props.noOfPlayers > 4}>Up</button>
+                </div>
+                
                 <button className="button button--small" onClick={this.props.onNext}>Next</button>
             </div>
         );
