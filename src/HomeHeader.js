82b49893915f71a1f25147f3c31e3ea133788338
Stuart Lambon <stuart@lambon.co.uk>
2018-05-29 22:34:27 +0100

    Switch homepage and footer logo for updated version

--- src/HomeHeader.js
+++ src/HomeHeader.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import illustration from './assets/img/illustration.svg';
+import logo from './assets/img/logo.svg';
 
 class HomeHeader extends Component {
   render() {
     return (
 		<div className="intro">
 			<h2 className="intro-header">
-				<span className="intro-header__subtitle">Get ready for the</span><br />
-				<span className="intro-header__title">Dead<br />Drop</span>
-			</h2>
+				<span className="intro-header__subtitle">Get ready for the</span><span className="intro-header__title hidden">Dead<br />Drop</span><br />
+                <img className="intro__logo" src={logo} alt="Dead Drop logo" />
+            </h2>
 			<img className="intro__illustration" src={illustration} alt="Dead Drop illustration showing an briefcase on a roadside" />
 		</div>
     );
