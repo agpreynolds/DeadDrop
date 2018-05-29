41fe88d53e4ae8cce4e9facf9bb96748c1d263a2
Stuart Lambon <stuart@lambon.co.uk>
2018-05-29 22:10:45 +0100

    Switch for to htmlFor on subscribe form email label

--- src/SubscribeForm.js
+++ src/SubscribeForm.js
@@ -2,23 +2,23 @@ import React, { Component } from 'react';
 
 class SubscribeForm extends Component {
   render() {
-    return (	    
+    return (
         <div className="subscribe">
-	        	<p className="subscribe-info">Dead Drop is a fun, competitive card game currently in development. Sign up for the newsletter to be a part of the story and to be the first to know about backer only discounts!</p>
-			
+	        <p className="subscribe-info">Dead Drop is a fun, competitive card game currently in development. Sign up for the newsletter to be a part of the story and to be the first to know about backer only discounts!</p>
+
 			<hr />
-			
+
 	        <form className="subscribe-form">
-	        		<label for="email">Email address:</label><br />
-		        
+	        	<label htmlFor="email">Email address:</label><br />
+
 		        <div className="input-group input-group--email">
-		        		<input type="email" name="email" />
+		        	<input type="email" name="email" />
 					<button type="submit" value="submit" aria-label="Submit" className="button button--white input-group__button"></button>
 				</div>
-				
+
 				<p className="update-info sans small">Updates are every 4–6 weeks and will cover the processes of development, funding and manufacture for this game!</p>
-	        </form> 
-	    </div> 
+	        </form>
+	    </div>
     );
   }
 }
