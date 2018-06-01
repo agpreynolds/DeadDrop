d08098879be6df7eecac731c154ba5bb7a3c9a4f
Stuart Lambon <stuart@lambon.co.uk>
2018-06-01 19:53:19 +0100

    Change footer.js email link

--- src/Footer.js
+++ src/Footer.js
@@ -10,7 +10,7 @@ class Footer extends Component {
     			<img className="logo" src={logo} alt="Dead Drop logo" />
                 <ul className="social">
                     <li><a href="https://twitter.com/deaddropthegame" title="Follow Dead Drop on Twitter" target="_blank" rel="noopener noreferrer"><i className="icon icon--white-twitter icon--large"></i></a></li>
-                    <li><a href="mailto:?subject=Get ready for the Dead Drop&amp;body=Dead Drop is a fun, competitive card game currently in development. Sign up for the newsletter to be a part of the story and to be the first to know about backer only discounts! https://deaddropthegame.com/" title="Share by email"><i className="icon icon--white-email icon--large"></i></a></li>
+                    <li><a href="mailto:contact@deaddropthegame.com" title="Send an email"><i className="icon icon--white-email icon--large"></i></a></li>
                 </ul>
     		</div>
         </footer>
